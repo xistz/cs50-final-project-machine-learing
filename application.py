bb3b115a4af0a57a7a47b37675d50efb3c2f715d
Chan Jian Hui Jonathan <tmxistz@gmail.com>
2018-06-18 06:55:13 +0000

    Add functionality to display uploaded file

--- application.py
+++ application.py
@@ -1,24 +1,17 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 app = Flask(__name__)
 
 UPLOAD_FOLDER = os.path.basename("uploads")
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     # clear all files from uploads directory
-    instruction = "Upload a picture of a flower and find out its name!"
-    return render_template("index.html", instruction=instruction, show_picture=False)
-
-@app.route("/upload", methods=["GET", "POST"])
-def upload():
-    # get picture of flower
-    # get topk from predict
-    # get a name of flower from topk and get description
     if request.method == "POST":
         # check if there is a file being uploaded
         picture = request.files["file"]
+        # check if submitted file is a jpeg
         picture_name = os.path.join(app.config['UPLOAD_FOLDER'], picture.filename)
         picture.save(picture_name)
         name = "Flower name"
@@ -29,8 +22,13 @@ def upload():
             instruction=instruction,
             show_picture=True,
             name=name,
-            picture="#",
+            picture=picture_name,
             description=description
             )
-    instruction = "Please choose a picture of a flower to upload!"
-    return render_template("index.html", instruction=instruction)
+    else:
+        instruction = "Upload a picture of a flower and find out its name!"
+        return render_template("index.html", instruction=instruction, show_picture=False)
+
+@app.route("/uploads/<filename>")
+def send_file(filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
